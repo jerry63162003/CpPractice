0dcbb104f52650846a7a6362a6dcbf0dcaa28c3d
dev10001 fh <jerry36162003@gmail.com>
2019-05-15 12:43:12 +0800

    Initial Commit

---
+++ CpPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CpPractice
+//
+//  Created by dev10001 fh on 15/05/2019.
+//  Copyright © 2019 dev10001 fh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CpPracticeTests/CpPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  CpPracticeTests.swift
+//  CpPracticeTests
+//
+//  Created by dev10001 fh on 15/05/2019.
+//  Copyright © 2019 dev10001 fh. All rights reserved.
+//
+
+import XCTest
+@testable import CpPractice
+
+class CpPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
